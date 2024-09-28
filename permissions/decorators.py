e8b0dbee8a0539685cfc0a684a390c40fd670807
JakubPoblocki <jakubpoblocki1@gmail.com>
2024-09-29 00:00:30 +0200

    dicts: Updated check permissions wrapper

--- permissions/decorators.py
+++ permissions/decorators.py
@@ -3,7 +3,7 @@ from functools import wraps
 from django.http import HttpResponseForbidden
 
 
-def permission_required(permission_code, action):
+def required_permission(permission_code, action):
     """
     Decorator to check if the user has the specified permission.
     Usage:
@@ -16,10 +16,19 @@ def permission_required(permission_code, action):
         @wraps(view_func)
         def _wrapped_view(request, *args, **kwargs):
             user = request.user
+            permission = user.permissions.filter(permission_code=permission_code)
 
-            has_permission = user.permissions.filter(permission_code=permission_code, action=action).exists()
+            if permission is None:
+                return HttpResponseForbidden("You do not have permission to perform this action.")
 
-            if not has_permission:
+            actions_mapping = {
+                'READ': permission.can_read,
+                'WRITE': permission.can_write,
+                'DELETE': permission.can_delete
+            }
+
+            has_permission = actions_mapping.get(action)
+            if has_permission is None or not has_permission:
                 return HttpResponseForbidden("You do not have permission to perform this action.")
 
             return view_func(request, *args, **kwargs)
